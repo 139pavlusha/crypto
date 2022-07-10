e1770929e0ecda432a00131f3a4441377f773a2c
139pavlusha <pavel1grek@gmail.com>
2022-07-10 22:30:20 +0300

    dropdown fix

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
-let $options = document.querySelectorAll('ul label');
-for (let i = 0; i < $options.length; i++) {
-  $options[i].addEventListener('click', () => {
-    document.querySelectorAll('details')[0].removeAttribute("open")
-    document.querySelectorAll('details')[1].removeAttribute("open")
+let $options = document.querySelectorAll('li');
+let $radio = document.querySelectorAll('input[type=radio]');
+
+for (let i = 0; i < 25; i++) {
+  $radio[i].addEventListener('click', () => {
+    if (!document.querySelectorAll('details')[0].hasAttribute("open") && $radio[i].checked)
+    console.log('-')
+      document.querySelectorAll('details')[0].setAttribute("open", "open")
   });
 }
+
+for (let i = 25; i < 50; i++) {
+  $radio[i].addEventListener('click', () => {
+    if (!document.querySelectorAll('details')[1].hasAttribute("open") && $radio[i].checked)
+      document.querySelectorAll('details')[1].setAttribute("open", "open")
+  });
+}
+
+
+// document.addEventListener('click', () => {
+//     if (document.querySelectorAll('details')[0].hasAttribute("open") || document.querySelectorAll('details')[1].hasAttribute("open")){
+//       document.querySelectorAll('details')[0].removeAttribute("open")
+//       document.querySelectorAll('details')[1].removeAttribute("open")
+//     }
+//   });
+
+
+
