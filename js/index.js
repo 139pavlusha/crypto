344bbbb6fa02bae2d90d074e85a91220b98b4032
139pavlusha <pavel1grek@gmail.com>
2022-07-10 21:25:55 +0300

    dropdown change

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let $options = document.querySelectorAll('ul.list label');
+for (let i = 0; i < $options.length; i++) {
+  $options[i].addEventListener('click', () => {
+    document.querySelectorAll('details')[0].removeAttribute("open")
+    document.querySelectorAll('details')[1].removeAttribute("open")
+  });
+}
