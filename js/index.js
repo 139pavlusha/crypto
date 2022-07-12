75110eb8f9f2d1b6531867f1369029ae9752c480
139pavlusha <pavel1grek@gmail.com>
2022-07-13 02:00:48 +0300

    footer and select fix

--- js/index.js
+++ js/index.js
@@ -1,28 +1,65 @@
-let $options = document.querySelectorAll('li');
+let clickerCounter = 0;
+
+let $optionsList = document.querySelectorAll('.custom-select ul');
 let $radio = document.querySelectorAll('input[type=radio]');
+let $selects = document.querySelectorAll('.select-input');
 
-for (let i = 0; i < 25; i++) {
-  $radio[i].addEventListener('click', () => {
-    if (!document.querySelectorAll('details')[0].hasAttribute("open") && $radio[i].checked)
-    console.log('-')
-      document.querySelectorAll('details')[0].setAttribute("open", "open")
-  });
+function rewriteSelects () {
+  for (let i = 0; i < 50; i++) {
+    if ($radio[i].checked){
+      // $selects[1].removeChild($selects[1].firstChild);
+      if (i < 25) 
+      {
+        if ($selects[0].firstChild != null) $selects[0].removeChild($selects[0].firstChild);
+        $selects[0].insertAdjacentHTML('beforeend', $radio[i].title);
+      }
+      else 
+      {
+        if ($selects[1].firstChild != null) $selects[1].removeChild($selects[1].firstChild);
+        $selects[1].insertAdjacentText('beforeend', $radio[i].title);
+      }
+    }
+  }
 }
+rewriteSelects();
 
-for (let i = 25; i < 50; i++) {
-  $radio[i].addEventListener('click', () => {
-    if (!document.querySelectorAll('details')[1].hasAttribute("open") && $radio[i].checked)
-      document.querySelectorAll('details')[1].setAttribute("open", "open")
-  });
-}
+let selectOpen1 = false
+$selects[0].addEventListener('click', () => {
+  $optionsList[1].className = 'list-close';
+  selectOpen2 = false
+  document.getElementById('select-input-buy').className = "select-input";
+  if (selectOpen1) $optionsList[0].className = 'list-close';
+  else $optionsList[0].className = '';
+  if (selectOpen1) document.getElementById('select-input-sell').className = "select-input";
+  else document.getElementById('select-input-sell').className = "select-input rotate";
+  selectOpen1 = !selectOpen1;
+  rewriteSelects();
+});
 
+let selectOpen2 = false
+$selects[1].addEventListener('click', () => {
+  $optionsList[0].className = 'list-close';
+  selectOpen1 = false
+  document.getElementById('select-input-sell').className = "select-input";
+  if (selectOpen2) $optionsList[1].className = 'list-close';
+  else $optionsList[1].className = '';
+  if (selectOpen2) document.getElementById('select-input-buy').className = "select-input";
+  else document.getElementById('select-input-buy').className = "select-input rotate";
+  selectOpen2 = !selectOpen2;
+  rewriteSelects();
+});
 
-// document.addEventListener('click', () => {
-//     if (document.querySelectorAll('details')[0].hasAttribute("open") || document.querySelectorAll('details')[1].hasAttribute("open")){
-//       document.querySelectorAll('details')[0].removeAttribute("open")
-//       document.querySelectorAll('details')[1].removeAttribute("open")
-//     }
-//   });
+document.addEventListener('click', function(event) {
+  if (!$selects[1].contains(event.target) && !$selects[0].contains(event.target)) {
+    selectOpen1 = selectOpen2 = false;
+    $optionsList[0].className = 'list-close';
+    $optionsList[1].className = 'list-close';
+    document.getElementById('select-input-sell').className = "select-input";
+    document.getElementById('select-input-buy').className = "select-input";
+    rewriteSelects();
+  }
+});
+
 
 
 
